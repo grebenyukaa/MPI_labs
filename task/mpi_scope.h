23717a3b806efe0a541b8c6eb9f774ba1c41ad5d
amadeus <am-vi@yandex.ru>
2016-01-22 22:40:04 +0300

    trace still not working

--- task/mpi_scope.h
+++ task/mpi_scope.h
@@ -1,9 +1,7 @@
 #ifndef MPI_SCOPE_H
 #define MPI_SCOPE_H
 
-#ifdef MPITV_ENABLED
-#include<pcontrol.h>
-#endif
+#include "mpi_trace.h"
 
 class MPI_Scope
 {
@@ -24,25 +22,6 @@ public:
         ClSend = 0x00FF00,
         ClProbe = 0x0000FF
     };
-
-    //checked on cluster -- was unable to find anything regarding mpicxx-TV
-#ifndef MPITV_ENABLED
-    enum PControlCodes
-    {
-        PC_TRACEEVENT = 8,
-        PC_TRARELEVEL = 6,
-        PC_TRACENODE  = 3,
-        PC_TRACEFILES = 101
-    };
-#else
-    enum PControlCodes
-    {
-        PC_TRACEEVENT = TRACEEVENT,
-        PC_TRARELEVEL = TRACELEVEL,
-        PC_TRACENODE  = TRACENODE,
-        PC_TRACEFILES = TRACEFILES
-    };
-#endif
 
     MPI_Trace(unsigned int color);
     ~MPI_Trace();
--- task/mpi_trace.cpp
+++ task/mpi_trace.cpp
@@ -2,26 +2,23 @@
 #include <cstddef>
 
 #include "mpi_scope.h"
-
-#ifdef MPITV_ENABLED
-#include<pcontrol.h>
-#endif
+#include "mpi_trace.h"
 
 const char* MPI_Trace::m_trace_file_name = "mpi_trace.trc";
 
 MPI_Trace::MPI_Trace(unsigned int color)
+    :
+    m_color(color)
 {
-    MPI::Pcontrol(MPI_Trace::PC_TRACEEVENT, "entry", color, 0, NULL);
+    MPI_Trace_Event(color, "entry");
 }
 
 MPI_Trace::~MPI_Trace()
 {
-    MPI::Pcontrol(MPI_Trace::PC_TRACEEVENT, "exit", m_color, 0, NULL);
+    MPI_Trace_Event(m_color, "exit");
 }
 
 void MPI_Trace::Init()
 {
-    MPI::Pcontrol(MPI_Trace::PC_TRACEFILES, NULL, m_trace_file_name, 0);
-    MPI::Pcontrol(MPI_Trace::PC_TRARELEVEL, 1, 1, 1);
-    MPI::Pcontrol(MPI_Trace::PC_TRACENODE, 1000000, 1, 1);
+    MPI_Trace_Init(m_trace_file_name);
 }
